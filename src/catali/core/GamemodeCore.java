2df47d993718255ed6f9b7eaae4af0cf92e13f75
0infinitive0 <emiyashiro2002@gmail.com>
2024-12-19 21:46:29 +0700

    catali 7

--- src/catali/core/GamemodeCore.java
+++ src/catali/core/GamemodeCore.java
@@ -169,6 +169,7 @@ public class GamemodeCore {
     }
 
     public void handleTeamEarnXp(int teamId, int amount) {
+        Log.info(getTeam(teamId));
         player.sendMessage(teams.getTeam(teamId));
         teams.getTeam(teamId).earnExp(amount);
     }
